6f65a4cdf31e2471137f05815bbae13ef626d292
HyRALis <petar.trajanoski.pt@gmail.com>
2021-11-08 07:53:25 +0100

    more settup

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <landing>
+  <landing/>
 </template>
 
 <script>
---
+++ src/modules/dummyData.js
@@ -0,0 +1,117 @@
+export const sections = [
+    {
+        label: 'Section One'
+    },
+    {
+        label: 'Section Two'
+    },
+    {
+        label: 'Section Three'
+    },
+    {
+        label: 'Section Four'
+    }
+];
+
+export const footerListOne = [
+    {
+        label: 'Footer Link',
+        url: 'https://valtech.com'
+    },
+    {
+        label: 'Footer Link',
+        url: 'https://valtech.com'
+    },
+    {
+        label: 'Footer Link',
+        url: 'https://valtech.com'
+    },
+    {
+        label: 'Footer Link',
+        url: 'https://valtech.com'
+    },
+    {
+        label: 'Footer Link',
+        url: 'https://valtech.com'
+    }
+];
+
+export const footerListTwo = [
+    {
+        label: 'Footer Link',
+        url: 'https://valtech.com'
+    },
+    {
+        label: 'Footer Link',
+        url: 'https://valtech.com'
+    },
+    {
+        label: 'Footer Link',
+        url: 'https://valtech.com'
+    },
+    {
+        label: 'Footer Link',
+        url: 'https://valtech.com'
+    },
+    {
+        label: 'Footer Link',
+        url: 'https://valtech.com'
+    }
+];
+
+export const footerListThree = [
+    {
+        label: 'Footer Link',
+        url: 'https://valtech.com'
+    },
+    {
+        label: 'Footer Link',
+        url: 'https://valtech.com'
+    },
+    {
+        label: 'Footer Link',
+        url: 'https://valtech.com'
+    },
+    {
+        label: 'Footer Link',
+        url: 'https://valtech.com'
+    },
+    {
+        label: 'Footer Link',
+        url: 'https://valtech.com'
+    }
+];
+
+export const articles = [
+    {
+        title: 'Teaser one',
+        image: '../assets/images/large-emerging technologies.png',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vel sem accumsan lorem vulputate lobortis a vitae leo. Maecenas ac euismod urna, sit amet fermentum ipsum. In hac habitasse platea dictumst. Nullam orci lacus, vulputate eget tellus vitae, eleifend egestas turpis. Curabitur ut placerat tellus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vel sem accumsan lorem vulputate lobortis a vitae leo. Maecenas ac euismod urna, sit amet fermentum ipsum. In hac habitasse platea dictumst. Nullam orci lacus, vulputate eget tellus vitae, eleifend egestas turpis. Curabitur ut placerat tellu Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vel sem accumsan lorem vulputate lobortis a vitae leo. Maecenas ac euismod urna, sit amet fermentum ipsum. In hac habitasse platea dictumst. Nullam orci lacus, vulputate eget tellus vitae, eleifend egestas turpis. Curabitur ut placerat tellus.'
+    },
+    {
+        title: 'Teaser two',
+        image: '../assets/images/large-emerging technologies.png',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    },
+    {
+        title: 'Teaser three',
+        image: '../assets/images/large-emerging technologies.png',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vel sem accumsan lorem vulputate lobortis a vitae leo. Maecenas ac euismod urna, sit amet fermentum ipsum. In hac habitasse platea dictumst. Nullam orci lacus, vulputate eget tellus vitae, eleifend egestas turpis. Curabitur ut placerat tellus'
+    },
+    {
+        title: 'Teaser four',
+        image: '../assets/images/large-emerging technologies.png',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vel sem accumsan lorem vulputate lobortis a vitae leo. In hac habitasse platea dictumst. Nullam orci lacus, vulputate eget tellus vitae, eleifend egestas turpis. Curabitur ut placerat tellus'
+    }
+];
+
+export const members = [
+    {
+        name: 
+        Sur
+    }
+]
